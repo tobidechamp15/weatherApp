e374440bfb4021625c90e61d0e187a989d5add5d
Oluwadare Oluwatobiloba <112389225+tobidechamp15@users.noreply.github.com>
2023-08-01 11:08:57 +0100

    user can search for city

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 const apiKey = "3e300be3558d50948cd69d2514d50a1a";
 const apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=bangalore";
+  "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
 
-async function checkWeather() {
-  const response = await fetch(apiUrl + `&appid=${apiKey}`);
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
   let data = await response.json();
 
   console.log(data);
@@ -15,3 +18,8 @@ async function checkWeather() {
 }
 
 checkWeather();
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
+  
