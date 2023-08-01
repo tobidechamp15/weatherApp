c54538376c79bb4318f71d18762524cf5f68e0f6
Olubukola Oluwadare <Olubukola.Oluwadare@ubagroup.com>
2023-08-01 23:43:06 +0100

    mobile-screen

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ const weatherIcon = document.querySelector(".weather-icon");
 async function checkWeather(city) {
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
-  if (response.status == 404||searchBox.value=="") {
+  if (response.status == 404) {
     document.querySelector(".error").style.display = "block";
     document.querySelector(".weather").style.display = "none";
   } else {
