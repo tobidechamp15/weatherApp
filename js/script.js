93fb218578703fcdb8ce0cb0083b5199d9f1938b
Oluwadare Oluwatobiloba <112389225+tobidechamp15@users.noreply.github.com>
2023-08-01 13:01:04 +0100

    image changing

--- js/script.js
+++ js/script.js
@@ -4,17 +4,38 @@ const apiUrl =
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city) {
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-  let data = await response.json();
 
-  console.log(data);
+  if (response.status == 404||searchBox.value=="") {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    let data = await response.json();
 
-  document.querySelector(".city").innerHTML = data.name;
-  document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
-  document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-  document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "assets/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "assets/clear.png";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "assets/rain.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "assets/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.src = "assets/mist.png";
+    }
+    document.querySelector(".weather").style.display = "block";
+        document.querySelector(".error").style.display = "none";
+
+  }
 }
 
 checkWeather();
@@ -22,4 +43,3 @@ checkWeather();
 searchBtn.addEventListener("click", () => {
   checkWeather(searchBox.value);
 });
-  
